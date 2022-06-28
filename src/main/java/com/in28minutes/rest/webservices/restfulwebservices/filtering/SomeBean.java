b1038987fe68f34c9b584c8156c8843777e3d94e
Mykola Manko <mankokolya@gmail.com>
2022-06-28 19:02:48 +0300

    Implement static filtering of a field

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SomeBean {
+    private String field1;
+    private String field2;
+    @JsonIgnore
+    private String field3;
+}
